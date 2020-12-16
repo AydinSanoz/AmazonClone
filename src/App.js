2f6b8a4cf5497fbd6d9661b1228c7b0bee939125
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-16 16:12:41 +0300

    header section created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Header from './pages/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Router>
+				<Switch>
+					<Route exact path="/About" component={About} />
+					<Route exact path="/" component={Home} />
+				</Switch>
+				
+			</Router>
+		</div>
+	);
 }
-
 export default App;
