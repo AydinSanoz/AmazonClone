ef56c57daa3e01efa8b9f68a2675a8ac1e76e6c3
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-17 01:46:08 +0300

    user auth check added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,49 @@ import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from './pages/Header';
-import Login from './pages/Login'
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import Checkout from './pages/Checkout';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { auth } from './firebaseUtils/firebase';
+import { useStateValue } from './context/StateProvider';
+
 
 function App() {
+	const [{user, basket},dispatch] = useStateValue()
+	useEffect(() => {
+		// will only run once when the app component loads...
+	
+		auth.onAuthStateChanged((authUser) => {
+		  console.log("THE USER IS >>> ", authUser);
+	
+		  if (authUser) {
+			// the user just logged in / the user was logged in
+	
+			dispatch({
+			  type: "SET_USER",
+			  user: authUser,
+			});
+		  } else {
+			// the user is logged out
+			dispatch({
+			  type: "SET_USER",
+			  user: null,
+			});
+		  }
+		});
+	  }, []);
+	  
 	return (
 		<div className="App">
 			<Router>
 				<Header />
 				<Switch>
-					<Route exact path="/Login" component={Login} />
-					<Route exact path="/About" component={About} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/about" component={About} />
 					<Route exact path="/" component={Home} />
 				</Switch>
 			</Router>
---
+++ src/firebaseUtils/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDUC0weflYKRurmDn5K4J9yQ6mzomxvPdU',
+	authDomain: 'clone-4c2ad.firebaseapp.com',
+	projectId: 'clone-4c2ad',
+	storageBucket: 'clone-4c2ad.appspot.com',
+	messagingSenderId: '273327553318',
+	appId: '1:273327553318:web:02c15131f547d6bd1b1fe7',
+	measurementId: 'G-1HGMZYXM5X',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
